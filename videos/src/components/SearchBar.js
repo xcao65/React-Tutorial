2763fed66cf37d09bac3c0f0a76b857aecd7bdca
Xun Cao <xcao65@gatech.edu>
2019-03-28 19:26:09 -0500

    [video] Add search bar cm

--- videos/src/components/SearchBar.js
+++ videos/src/components/SearchBar.js
@@ -3,11 +3,15 @@ import React from 'react';
 class SearchBar extends React.Component {
   state = { term: '' };
 
-  onInputChange = event => {
+  onInputChange = event => {//event as props for event handler 
     this.setState({ term: event.target.value });
   };
 
+  // calls the callback function set in App.js
   onFormSubmit = event => {
+    // <form/> will submit auto if user hit enter key, page gonna refresh
+    // ensure browser do not auto submit form anytime user submitted it
+    // because we want to run custom logic
     event.preventDefault();
 
     this.props.onFormSubmit(this.state.term);
@@ -16,13 +20,15 @@ class SearchBar extends React.Component {
   render() {
     return (
       <div className="search-bar ui segment">
+        {/* do not put parenthesis on onFormSubmit, cause it is passed to props*/}
         <form onSubmit={this.onFormSubmit} className="ui form">
           <div className="field">
             <label>Video Search</label>
             <input
               type="text"
               value={this.state.term}
-              onChange={this.onInputChange}
+              // callback function will be called anytime input gets changed
+              onChange={this.onInputChange} 
             />
           </div>
         </form>
